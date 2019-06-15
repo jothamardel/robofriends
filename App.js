7398ac10faff249b72f6c6b9bdf1f121068ae04d
Bee <ardelmbiplang@gmail.com>
2019-06-15 12:16:45 +0100

    RoboFriends

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+
+
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state = { 
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value})
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
+        })
+        // console.log(this.setState({ searchfield: filteredRobots}))
+        // console.log(filteredRobots);
+    }
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
+        })
+        return(
+                <div className='tc'>
+                    <h1 className=''>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+        );
+
+    }
+
+}
+
+export default App;
